2179e02447b4b845c24ba83021cdd5e52d5f03b6
miguelConchacalla <miguel.conchacalla@ucsp.edu.pe>
2022-09-02 08:27:12 -0500

    programas con ifs 2

--- Semana2/anhobisiesto.cpp
+++ Semana2/anhobisiesto.cpp
@@ -8,7 +8,7 @@ int main()
     
     cout<<"Ingrese anho: ";   cin>>a;
 
-    if (a % 4 == 0){
+    if (a % 4 == 0 && a % 100 != 0 || a % 400 == 0){
         cout<<"Es una anho bisiesto";
     } else{
         cout<<"No es bisiesto";
